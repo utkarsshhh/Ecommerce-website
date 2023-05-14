73e8774ab4f232e2f397843249cfab84e41814bb
Utkarsh Padia <utkarshpadia@gmail.com>
2023-05-14 19:12:19 +0530

    initialise project

---
+++ main.py
@@ -0,0 +1,22 @@
+# Installing required libraries
+
+from flask import Flask, request
+from flask_cors import CORS
+import requests
+import pandas as pd
+import numpy as np
+import pickle
+import nltk
+import re
+from datetime import datetime,timedelta,date,timezone
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+app = Flask(__name__)
+CORS(app)
+
+
+
